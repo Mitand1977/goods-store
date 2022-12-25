af314b3ab9722b149f952c61f490d53b4eec9df5
Andrii <kznkv.test@gmail.com>
2022-12-25 22:03:34 +0200

    react.fragment vmesto div - 2 ways

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1>Hello React</h1>
+}
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, mollitia sunt. Totam minus necessitatibus id dolore,
+                facere, voluptatum libero sunt numquam tempore atque a illum
+                quisquam! Expedita autem nesciunt temporibus?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, mollitia sunt. Totam minus necessitatibus id dolore,
+                facere, voluptatum libero sunt numquam tempore atque a illum
+                quisquam! Expedita autem nesciunt temporibus?
+            </p>
+        </React.Fragment>
+    )
+}
+
 const App = () => {
     return (
-        <div>
-            <h1>Hello React</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Aperiam, mollitia sunt. Totam minus necessitatibus id dolore,
-                facere, voluptatum libero sunt numquam tempore atque a illum
-                quisquam! Expedita autem nesciunt temporibus?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Aperiam, mollitia sunt. Totam minus necessitatibus id dolore,
-                facere, voluptatum libero sunt numquam tempore atque a illum
-                quisquam! Expedita autem nesciunt temporibus?
-            </p>
-        </div>
+        <>
+            <Title />
+            <Content />
+        </>
     )
 }
 
